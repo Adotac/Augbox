f6c9d6ab99ce740f865bbd82434570d78a879117
Adotac <otacool89@gmail.com>
2023-03-30 15:30:58 +0800

    health test working

--- VR Game Test/Assets/Game/Scripts/Hand.cs
+++ VR Game Test/Assets/Game/Scripts/Hand.cs
@@ -1,9 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
-    [SerializeField] private GameObject followObject;
-    
+
+    [SerializeField]
+    private Player controlledPlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            try
+            {
+                Player enemyHealth = other.gameObject.GetComponent<Player>();
+
+                if (enemyHealth.Health < 0)
+                {
+                    enemyHealth.Health = enemyHealth.Health - controlledPlayer.Damage;
+                    //enemyHealth.refBar.UpdateValue();
+                    Debug.Log($"Enemy damaged by -{controlledPlayer.Damage}\nHealth Remaining: {enemyHealth.Health}");
+                }
+
+            }
+            catch (UnityException e)
+            {
+                Debug.Log($"Collision Error\n{e}");
+            }
+        }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //controlledPlayer.OnCollisionEnter(collision);
+        //Debug.Log("Hand on collision");
+
+        
+    }
 }
--- VR Game Test/Assets/Game/Scripts/Player.cs
+++ VR Game Test/Assets/Game/Scripts/Player.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float Health {get; set;} = 100;
+    public float Health {get; set;} = 100;
     [SerializeField]
-    private float Damage {get;set;} = 20;
+    public float Damage {get;set;} = 20;
 
     public ProgressBar refBar;
-    public Collider objCollider;
 
     // Start is called before the first frame update
     void Start()
@@ -21,23 +20,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if(TryGetComponent<MeshCollider>(out MeshCollider mc)){
-        //     mc.
-        // }
-        
+        refBar.UpdateValue(Health);
     }
-
-    private void OnCollisionEnter(Collision other) {
-        try{
-            Player enemyHealth = other.gameObject.GetComponent<Player>();
-            enemyHealth.refBar.UpdateValue(-Damage);
-            Debug.Log($"Enemy damaged by -{Damage}");
-        }
-        catch(UnityException e){
-            Debug.Log($"Collision Error\n{e}");
-        }
-
         
-    }
+    
 
 }
